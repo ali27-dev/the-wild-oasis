7582867878781512add0e0029baede91bd6c4565
ali27-dev <khanali3229@gmail.com>
2026-02-25 15:54:12 +0500

    In this project i want to use styled-component CSS for that,I'm install styled-components package and also implemnt little bit styling.And also i'm carete GlobalStyles componet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-weight: 800;
+  font-size: 30px;
+`;
+const Button = styled.button`
+  font-weight: 800;
+  font-size: 1rem;
+  border: none;
+  padding: 0.8rem 1.2rem;
+  background-color: var(--color-grey-800);
+  color: var(--color-grey-100);
+  border-radius: 1.2rem;
+`;
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Hello World!</H1>
+        <Button>Click me</Button>
+      </div>
+    </>
   );
 }
 
