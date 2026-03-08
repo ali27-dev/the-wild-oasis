2077b1cd4a7d8305ae13f5d0a57ad5214cfb2b68
ali27-dev <khanali3229@gmail.com>
2026-03-08 15:56:51 +0500

    Apply the render props pattern.

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -88,7 +88,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body(params) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 // function Footer(params) {}
 
 Table.Header = Header;
