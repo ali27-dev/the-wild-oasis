c3b309b0a83a507766bac3e0191c7b390dc2e044
ali27-dev <khanali3229@gmail.com>
2026-03-26 22:36:53 +0500

    Build the signup from.

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { logout as logoutApi } from "../../services/apiAuth";
 
 export function useLogout() {
   const navigate = useNavigate();
+  const queryClint = useQueryClient();
 
   const { mutate: logout, isLoading } = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
+      queryClint.removeQueries();
       navigate("/login", { replace: true });
     },
   });
