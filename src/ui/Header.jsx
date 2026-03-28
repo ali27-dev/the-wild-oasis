dc5726123d005c35b92363d9d8cb354e4870e8c1
ali27-dev <khanali3229@gmail.com>
2026-03-28 11:52:45 +0500

    Build the app Header component.

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,23 @@
 import styled from "styled-components";
-import LogOut from "../features/authentication/LogOut";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 2.5rem 2.8rem;
   border-bottom: 1px solid #000;
+
+  display: flex;
+  gap: 1.2rem;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      <LogOut />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
