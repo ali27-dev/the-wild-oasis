60f68a85eb2dd79aee22b94715da0eaccf4bf6b0
ali27-dev <khanali3229@gmail.com>
2026-03-29 14:56:56 +0500

    Compute recent bookings and stays.

---
+++ src/features/dashboard/useRecentStays.js
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getStaysAfterDate } from "../../services/apiBookings";
+
+export function useRecentStays() {
+  const [serachParams] = useSearchParams();
+
+  const numDays = !serachParams.get("last")
+    ? 7
+    : Number(serachParams.get("last"));
+
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: stays } = useQuery({
+    queryFn: () => getStaysAfterDate(queryDate),
+    queryKey: ["stays", `last-${numDays}`],
+  });
+
+  const confirmedStays = stays?.filter(
+    (stay) => stay.status === "check-in" || stay.status === "check-out"
+  );
+
+  return { isLoading, stays, confirmedStays };
+}
