f6f9b821311f39ec707b537e5321566c799af099
ali27-dev <khanali3229@gmail.com>
2026-03-29 12:11:35 +0500

    Implement Dark mode with CSS variables.

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import LogOut from "../features/authentication/LogOut";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle";
 const StyledHeaderMenu = styled.ul`
   display: flex;
   gap: 0.4rem;
@@ -16,6 +17,7 @@ function HeaderMenu() {
           <HiOutlineUser />
         </ButtonIcon>
       </li>
+      <DarkModeToggle />
       <li>
         <LogOut />
       </li>
